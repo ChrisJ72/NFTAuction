09d9a544e438a0803b6284aa9d4814bedf8fdf6f
Chris Johnson <chris.johnson@duke.edu>
2023-05-24 21:02:41 -0700

    bare bones contract

--- NFTAuction.sol
+++ NFTAuction.sol
@@ -3,5 +3,46 @@
 pragma solidity ^0.8.19;
 
 contract NFTAuction {
-    address public owner;
+    address public immutable owner;
+    uint256 public immutable minimumContribution;
+    address[] public bidders;
+    mapping(address => uint256) bidderToAmount;
+
+
+    constructor(uint256 _minimumContribution) {
+        owner = msg.sender;
+        minimumContribution = _minimumContribution;
+    }
+
+    function makeBid() public payable {
+        bidders.push(msg.sender);
+        bidderToAmount[msg.sender] += msg.value;
+    }
+
+    function finalizeMarket() public onlyOwner {
+        uint256 max;
+        address winnerAddress;
+        for (uint i=0; i < bidders.length; i++) {
+            address candidate = bidders[i];
+            if (bidderToAmount[candidate]>max) {
+                max = bidderToAmount[candidate];
+                winnerAddress = candidate;
+            }
+        }
+        (bool callSuccess,) = payable(winnerAddress).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    function revertMarket() public onlyOwner {
+        for (uint i=0; i < bidders.length; i++) {
+            address candidate = bidders[i];
+            (bool callSuccess,) = payable(candidate).call{value: bidderToAmount[candidate]}("");
+            require(callSuccess, "Call failed");
+        }
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 }
